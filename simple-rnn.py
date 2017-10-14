f38c2e07b7e8a564a0ccf95f703687da0f0ce826
Priyam Tejaswin <priyamtejaswin@gmail.com>
2017-10-14 09:25:41 +0530

    Added forward pass.

--- simple-rnn.py
+++ simple-rnn.py
@@ -10,8 +10,14 @@ from itertools import izip
 RANDOM_SEED = 1234
 np.random.seed(RANDOM_SEED)
 
-## data - counting 1's in a sequence
+
 def generate_data(length, size):
+    """
+    Generate a random sequence of 1's and 0's.
+    Inputs: length of sequences, number of samples
+    Outputs: list of sequences, sum of sequences
+    """
+
     print "Generating %d samples of length %d."%(size, length)
     data, target = [], []
 
@@ -28,14 +34,52 @@ def generate_data(length, size):
     return np.array(data), np.array(target)
 
 
-## main
+def relu(x):
+    """
+    ReLU activation,
+    Inputs: any number
+    Outputs: ReLU'd number
+    """
+
+    return np.max((0, x))
+
+
+def forward_pass((Wi, Wh, Wo), x):
+    """
+    Run single instance of a forward pass.
+    Inputs: (weights), x_input
+    Outputs: S_input, S_activation, y
+    """
+
+    aS = np.zeros(x.shape[0])
+    bS = np.zeros_like(aS)
+
+    for t in range(x.shape[0]):
+        aS[t] = (Wi * x[t]) + (Wh * bS[t-1]) # right now everything is one number
+        bS[t] = relu(aS[t])
+
+    y = Wo * bS[t]
+
+    return aS, bS, y
+
+
 def main():
-    T = 10
-    SIZE = 50
-    data, target = generate_data(length=T, size=SIZE)
+    """
+    Main code for the program.
+    """
+
+    LENGTH = 10
+    SIZE = 25
+    data, target = generate_data(length=LENGTH, size=SIZE)
+
+    SCALE = 0.1
+    Wi = np.random.rand() * SCALE
+    Wh = np.random.rand() * SCALE
+    Wo = np.random.rand() * SCALE
 
     for _d, _t in izip(data, target):
-        print _d, _t
+        aS, bS, y = forward_pass((Wi, Wh, Wo), _d)
+        print bS, _t, y
 
     return
 
