9d06f3882b978405a9ecb0491bbd08603288017f
priyamtejaswin <priyamtejaswin@gmail.com>
2017-12-26 23:50:28 +0530

    AbstractBase class. Will need to updated backward args.

--- layers/abstract_layer.py
+++ layers/abstract_layer.py
@@ -1,3 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
-class 
+class AbstractLayer(object):
+    """Abstract class for layers."""
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def forward(self, x):
+        return
+
+    @abstractmethod
+    def backward(self, current_error):
+        return
