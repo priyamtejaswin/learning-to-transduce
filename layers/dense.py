930faac0e4e35f6fab860017df41f04082948521
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-12-28 11:46:58 +0530

    Dense layer backward pass fixed 1. fixed shapes for backward pass 2. added python3 compatibility (YOUNEVERKNOW) 3. added shape check function in class to check if params.shape == params_grad.shape

--- layers/dense.py
+++ layers/dense.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from abstract_layer import AbstractLayer
 import numpy as np
 from copy import deepcopy
@@ -23,10 +24,17 @@ class Dense(AbstractLayer):
         also save, dL_dW, dL_db
         """
         self.output_grad    = deepcopy(current_error)
-        self.input_grad     = np.dot(self.output_grad, self.weights)
-        self.weights_grad   = np.dot(self.output_grad, self.input)
-        self.bias_grad      = self.output_grad * 1.0
+        self.input_grad     = np.dot(self.output_grad, self.weights.T)
+        self.weights_grad   = np.dot(self.input.T, self.output_grad)
+        self.bias_grad      = self.output_grad.sum(axis=0) 
+        self.check_grad_shapes() # built in self check on variables and their gradients  
         return self.input_grad
+
+    def check_grad_shapes(self):
+        assert self.input.shape == self.input_grad.shape, "input and input_grad shapes do not match"
+        assert self.output.shape == self.output_grad.shape, "output and output_grad shapes do not match"
+        assert self.weights.shape == self.weights_grad.shape, "weights and weights_grad shapes do not match"
+        assert self.bias.shape == self.bias_grad.shape, "bias and bias_grad shapes do not match"
 
     def return_weights(self):
         return [self.weights, self.bias]
@@ -35,12 +43,15 @@ class Dense(AbstractLayer):
         return [self.weights_grad, self.bias_grad]
 
 if __name__ == '__main__':
-    x = np.random.rand(2, 3) * 0.1
-    d = Dense(n_in=3, n_out=5)
 
-    import ipdb; ipdb.set_trace()
-    f = d.forward(x)
-    error = np.zeros_like(f) + 1.0
-
-    d.backward(error)
-    print "HERE"
+    try:
+        x = np.random.rand(5, 10) * 0.1
+        d = Dense(n_in=10, n_out=20)
+        # import ipdb; ipdb.set_trace()
+        f = d.forward(x)
+        error = np.zeros_like(f) + 1.0
+        d.backward(error)
+    except:
+        print("Backward pass failed") 
+    else:
+        print("Backward pass shapes check passed")
