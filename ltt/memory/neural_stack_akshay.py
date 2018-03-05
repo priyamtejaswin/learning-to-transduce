2cf3cb0ea726efb308ed34863e6d86d412e2ebca
Akshay Chawla <chawla.akshay1234@gmail.com>
2018-03-05 17:51:02 +0530

    Neural stack akshay - read operation + test with example from paper + Added read operation r_t + neural stack forward pass by separately calling V_t, s_t and r_t + WORKS! with example given in the paper.

--- ltt/memory/neural_stack_akshay.py
+++ ltt/memory/neural_stack_akshay.py
@@ -11,8 +11,8 @@ class NeuralStack():
     def V_t(self, V_prev, v_t):
         
         # checks and balances 
-        assert V_prev.shape[1] = v_t.shape[1] 
-        assert v_t.shape[0] = 1 
+        assert V_prev.shape[1] == v_t.shape[1] 
+        assert v_t.shape[0] == 1 
         # Concatenate v_t to V_prev, this now is the V_t
         return np.concatenate([V_prev, v_t])
 
@@ -41,5 +41,67 @@ class NeuralStack():
         return np.array(s_curr)
 
 
-    def r_t(self):
-        pass 
+    def r_t(self, s_t, V_t):
+        # infer current time step 
+        CURTIMESTEP = len(s_t)
+        EMBEDDINGSIZE = V_t.shape[1]
+        print("Current timestep: ", CURTIMESTEP)
+        print("Embedding size: ", EMBEDDINGSIZE)
+        
+        # checks and balances 
+        assert len(s_t) == len(V_t)
+        
+        # abstraction for convenience 
+        def r_t_i(i):
+            return V_t[i] * (np.minimum(s_t[i], np.maximum(0, 1 - np.sum(s_t[i+1:]))))
+        
+        # looping
+        weighted_r = [] 
+        for i in range(CURTIMESTEP):
+            weighted_r.append(r_t_i(i))
+        weighted_r = np.array(weighted_r) 
+        
+        # checks and balances 
+        assert np.shape(weighted_r) == (CURTIMESTEP, EMBEDDINGSIZE)
+        
+        # do the weighted sum 
+        r_curr = np.sum(weighted_r, axis=0, keepdims=True)
+        assert np.shape(r_curr) == (1, EMBEDDINGSIZE) 
+        
+        return r_curr
+
+def test_stack_forward():
+    EMBEDDINGSIZE = 3 
+    V = {} 
+    s = {} 
+    r = {} 
+    V[0] = np.empty(shape=(0, EMBEDDINGSIZE))
+    s[0] = np.array([])
+    r[0] = np.array([])
+    ns = NeuralStack() 
+
+    vts = np.eye(3)
+
+    # t = 1 
+    V[1] = ns.V_t( V[0], vts[0].reshape(1,-1) )
+    s[1] = ns.s_t( s[0], 0, 0.8 ) 
+    r[1] = ns.r_t( s[1], V[1] ) 
+
+    # t = 2 
+    V[2] = ns.V_t( V[1], vts[1].reshape(1,-1) )
+    s[2] = ns.s_t( s[1], 0.1, 0.5 ) 
+    r[2] = ns.r_t( s[2], V[2] ) 
+
+    # t = 3
+    V[3] = ns.V_t( V[2], vts[2].reshape(1,-1) )
+    s[3] = ns.s_t( s[2], 0.9, 0.9 ) 
+    r[3] = ns.r_t( s[3], V[3] ) 
+
+    print("Last read vector: ", r[3])
+    print("Last stack state: \n", V[3])
+    print("Last strength vector: ", s[3])
+
+    import ipdb; ipdb.set_trace()
+
+
+
