721e395863e22ea03ab0c2cdd73d3ba441a996e6
priyamtejaswin <priyamtejaswin@gmail.com>
2018-03-20 02:36:17 +0530

    Added temporary file for the controller. - Wrote skeleton code. - Added `class Controller`. - Assertions against skeletal code pass.
     Changes to be committed:
    	modified:   controller_test.py

--- controller_test.py
+++ controller_test.py
@@ -47,10 +47,62 @@ nstack = NeuralStack(name="nstack")
 
 import ipdb; ipdb.set_trace()
 
-nstack.forward(V_prev=cnt_input[2][0], s_prev=cnt_input[2][1],
+V_t, s_t, r_t = nstack.forward(V_prev=cnt_input[2][0], s_prev=cnt_input[2][1],
                d_t = sigd_dt.forward(dense_dt.forward(current_output)).squeeze(),
                u_t = sigd_ut.forward(dense_ut.forward(current_output)).squeeze(),
                v_t = tanh_vt.forward(dense_vt.forward(current_output))
 )
 
-print("PASSED.")
+#### Main controller class.
+class Controller:
+    """ Main Controller class. """
+
+    def __init__(self, rnn_in, rnn_hid, rnn_out, LENGTH, EMBED_SIZE, name="controller"):
+        """ Initializes the rnn, stack and all dense+nonlinear objects. """
+
+        self.rnn = RNN(n_in=rnn_in, n_hidden=rnn_hid, n_out=rnn_out, LENGTH=LENGTH, name="recurrent_layer")
+
+        self.dense_dt = Dense(n_in=rnn_out, n_out=1, name="dense_dt")
+        self.dense_ut = Dense(n_in=rnn_out, n_out=1, name="dense_ut")
+        self.dense_vt = Dense(n_in=rnn_out, n_out=EMBED_SIZE, name="dense_vt")
+        self.dense_ot = Dense(n_in=rnn_out, n_out=EMBED_SIZE, name="dense_ot")
+
+        self.sigd_dt = Sigmoid(name="sigd_dt")
+        self.sigd_ut = Sigmoid(name="sigd_ut")
+        self.tanh_vt = Tanh(name="tanh_vt")
+        self.tanh_ot = Tanh(name="tanh_ot")
+
+        self.nstack = NeuralStack(name="nstack")
+
+    def step_forward(self, cnt_input, seq_input):
+        """
+        cnt_input is the tuple as defined in the paper.
+        seq_input is a (1, EMBED_SIZE) vector.
+        """
+
+        rnn_h_prev, stack_r_prev = cnt_input[0], cnt_input[1]
+        V_prev, s_prev = cnt_input[2]
+
+        rnn_i_curr = np.hstack((seq_input, stack_r_prev))
+        rnn_h_curr, rnn_o_curr = self.rnn.step_forward(xin=rnn_i_curr, hp=rnn_h_prev)
+
+        V_curr, s_curr, r_curr = self.nstack.forward(
+                V_prev=V_prev, s_prev=s_prev,
+                d_t = self.sigd_dt.forward(self.dense_dt.forward(rnn_o_curr)).squeeze(),
+                u_t = self.sigd_ut.forward(self.dense_ut.forward(rnn_o_curr)).squeeze(),
+                v_t = self.tanh_vt.forward(self.dense_vt.forward(rnn_o_curr))
+        )
+
+        o_t = self.tanh_ot.forward(self.dense_ot.forward(rnn_o_curr))
+
+        return (rnn_h_curr, r_curr, (V_curr, s_curr)), o_t
+
+#### Testing.
+import ipdb; ipdb.set_trace()
+CTRL = Controller(rnn_in=4, rnn_hid=3, rnn_out=5, LENGTH=10, EMBED_SIZE=2)
+next_state, cnt_output = CTRL.step_forward(cnt_input, sequence_input[0])
+
+assert np.array_equal(V_t, next_state[2][0])
+assert np.array_equal(s_t, next_state[2][1])
+assert np.array_equal(r_t, next_state[1])
+print("PASSED")
